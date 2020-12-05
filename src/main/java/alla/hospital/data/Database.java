684d97d2c69a189c567e477acecaba7627faf6bc
Alexander Golovko <alik@csltd.com.ua>
2020-12-05 22:12:44 +0200

    Чтение БД из файла. Добавлено экранирование символа-разделителя.

--- src/main/java/alla/hospital/data/Database.java
+++ src/main/java/alla/hospital/data/Database.java
@@ -5,9 +5,12 @@ import alla.hospital.model.Doctor;
 import alla.hospital.model.Sick;
 
 import java.io.*;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Database {
     private static final Database instance = new Database();
@@ -20,10 +23,6 @@ public class Database {
     public static Database getInstance() {
         return instance;
     }
-
-    public void readFromFile(String fileName) {
-
-    };
 
     public List<Sick> getSicks() {
         if (sicks==null) sicks = new ArrayList<>();
@@ -40,29 +39,139 @@ public class Database {
         return doctors;
     }
 
+    private String escape(String s) {
+        return s==null ? "" : s.replaceAll("\\|", "&777;");
+    }
+
+    private String escape(Date d) {
+        return d==null ? "" : simpleDateFormat.format(d);
+    }
+
+    private String escape(Integer i) {
+        return i==null ? "" : String.valueOf(i);
+    }
+
+    private String unescapeString(String s) {
+        return s==null || s.isEmpty() ? null : s.replaceAll("&777;", "|");
+    }
+
+    private Date unescapeDate(String s) throws ParseException {
+        return s==null || s.isEmpty() ? null : simpleDateFormat.parse(s);
+    }
+
+    private Integer unescapeInteger(String s) throws ParseException {
+        return s==null || s.isEmpty() ? null : Integer.parseInt(s);
+    }
+
+
     public void saveToFile(String fileName) throws IOException {
         FileWriter fileWriter = new FileWriter(fileName);
-        fileWriter.write("Version;1\n");
-        fileWriter.write("Bulks;" + bulks.size()+'\n');
+        fileWriter.write("Version|1\n");
+        fileWriter.write("Bulks|" + bulks.size()+'\n');
         for (Bulk tmpBulk : bulks)
         {
-            String s = String.format("%d;%s", tmpBulk.getId(), tmpBulk.getName());
+            String s = String.format("%s|%s",
+                    escape(tmpBulk.getId()),
+                    escape(tmpBulk.getName()));
             fileWriter.write(s + '\n');
         }
-        fileWriter.write("Doctors;" + doctors.size()+'\n');
+        fileWriter.write("Doctors|" + doctors.size()+'\n');
         for (Doctor tmpDoctor : doctors)
         {
-            String s = String.format("%d;%s;%s;%s;%s;%s", tmpDoctor.getId(), tmpDoctor.getFirstName(), tmpDoctor.getMiddleName(), tmpDoctor.getLastName(), tmpDoctor.getPhone(), tmpDoctor.getAddress());
+            String s = String.format("%s|%s|%s|%s|%s|%s",
+                    escape(tmpDoctor.getId()),
+                    escape(tmpDoctor.getFirstName()),
+                    escape(tmpDoctor.getMiddleName()),
+                    escape(tmpDoctor.getLastName()),
+                    escape(tmpDoctor.getPhone()),
+                    escape(tmpDoctor.getAddress()));
             fileWriter.write(s + '\n');
         }
-        fileWriter.write("Sicks;" + sicks.size()+'\n');
+        fileWriter.write("Sicks|" + sicks.size()+'\n');
         for (Sick tmpSick : sicks)
         {
-            String s = String.format("%d;%s;%s;%s;%s;%s;%s;%s;%d;%d",
-                    tmpSick.getId(), tmpSick.getFirstName(), tmpSick.getMiddleName(), tmpSick.getLastName(), tmpSick.getPhone(), tmpSick.getAddress(),
-                    simpleDateFormat.format(tmpSick.getBirthday()), tmpSick.getDiagnosis(), tmpSick.getDoctorId(), tmpSick.getBulkId());
+            String s = String.format("%s|%s|%s|%s|%s|%s|%s|%s|%s|%s",
+                    escape(tmpSick.getId()),
+                    escape(tmpSick.getFirstName()),
+                    escape(tmpSick.getMiddleName()),
+                    escape(tmpSick.getLastName()),
+                    escape(tmpSick.getPhone()),
+                    escape(tmpSick.getAddress()),
+                    escape(tmpSick.getBirthday()),
+                    escape(tmpSick.getDiagnosis()),
+                    escape(tmpSick.getDoctorId()),
+                    escape(tmpSick.getBulkId()));
             fileWriter.write(s + '\n');
         }
         fileWriter.flush();
     }
+
+    public void readFromFile(String fileName) {
+        List<Sick> tmpSicks = new ArrayList<>();
+        List<Bulk> tmpBulks = new ArrayList<>();
+        List<Doctor> tmpDoctors = new ArrayList<>();
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            Scanner scanner = new Scanner(fileReader);
+            assert scanner.hasNextLine();
+            String s = scanner.nextLine();
+            String[] splited = s.split("\\|",2);
+            assert "Version".equals(splited[0]) && "1".equals(splited[1]);
+            while (scanner.hasNextLine()) {
+                s = scanner.nextLine();
+                splited = s.split("\\|",2);
+                String model = splited[0];
+                int count = Integer.parseInt(splited[1]);
+                if ("Doctors".equals(model)) {
+                    for (int i = 0; i < count; i++) {
+                        assert scanner.hasNextLine();
+                        s = scanner.nextLine();
+                        splited = s.split("\\|",6);
+                        Doctor doctor = new Doctor(
+                                unescapeInteger(splited[0]),
+                                unescapeString(splited[1]),
+                                unescapeString(splited[2]),
+                                unescapeString(splited[3]),
+                                unescapeString(splited[4]),
+                                unescapeString(splited[5]));
+                        tmpDoctors.add(doctor);
+                    }
+                } else if ("Bulks".equals(model)) {
+                    for (int i = 0; i < count; i++) {
+                        assert scanner.hasNextLine();
+                        s = scanner.nextLine();
+                        splited = s.split("\\|",2);
+                        Bulk bulk = new Bulk(
+                                unescapeInteger(splited[0]),
+                                unescapeString(splited[1]));
+                        tmpBulks.add(bulk);
+                    }
+                } else if ("Sicks".equals(model)) {
+                    for (int i = 0; i < count; i++) {
+                        assert scanner.hasNextLine();
+                        s = scanner.nextLine();
+                        splited = s.split("\\|",10);
+                        assert splited.length == 10;
+                        Sick sick = new Sick(
+                                unescapeInteger(splited[0]),
+                                unescapeString(splited[1]),
+                                unescapeString(splited[2]),
+                                unescapeString(splited[3]),
+                                unescapeString(splited[4]),
+                                unescapeString(splited[5]),
+                                unescapeDate(splited[6]),
+                                unescapeString(splited[7]),
+                                unescapeInteger(splited[8]),
+                                unescapeInteger(splited[9]));
+                        tmpSicks.add(sick);
+                    }
+                }
+            }
+            this.sicks = tmpSicks;
+            this.doctors = tmpDoctors;
+            this.bulks = tmpBulks;
+        } catch (AssertionError | Exception e) {
+            throw new RuntimeException("Error reading of database from file " + fileName, e);
+        }
+    };
 }
--- src/test/java/alla/hospital/data/DatabaseTest.java
+++ src/test/java/alla/hospital/data/DatabaseTest.java
@@ -5,6 +5,7 @@ import alla.hospital.model.Doctor;
 import alla.hospital.model.Sick;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
@@ -16,6 +17,11 @@ public class DatabaseTest {
 
     @Test
     public void readFromFile() {
+        Database database = Database.getInstance();
+        database.readFromFile("database.txt");
+        System.out.println(database.getBulks().size());
+        System.out.println(database.getDoctors().size());
+        System.out.println(database.getSicks().size());
     }
 
     private static Date getDate(int year, int month, int day) {
@@ -29,14 +35,15 @@ public class DatabaseTest {
         Database database = Database.getInstance();
         List<Bulk> bulks = database.getBulks();
         List<Doctor> doctors = database.getDoctors();
-        bulks.add(new Bulk(1, "Койка 1"));
-        bulks.add(new Bulk(2, "Койка 2"));
-        doctors.add(new Doctor(1, "Сергей", "Иванович", "Иванов", "031", null));
+        bulks.add(new Bulk(1, "Койка|1"));
+        bulks.add(new Bulk(2, "Койка#2"));
+        doctors.add(new Doctor(1, "Сергей", "Иванович", "Иванов", "031", "null|address"));
         doctors.add(new Doctor(2, "Алла", "Сергеевна", "Иванова", "032", null));
         doctors.add(new Doctor(3, "Борис", "Петрович", "Павлов", "033", null));
+        doctors.add(new Doctor(4, "Иван", null, null, null, null));
         List<Sick> sicks = database.getSicks();
         sicks.add(new Sick(1, "A", "L", "M", "phone", "address", getDate(2020,0,1), "diagnosis", 1, 1));
-        sicks.add(new Sick(2, "firstName", "LastName", "Mname", "phone2", "address2", getDate(1970, 5, 6), "COVID", 1, 1));
+        sicks.add(new Sick(2, "firstName", "LastName", "Mname", "phone2", "address2", null, "COVID", null, null));
         database.saveToFile("database.txt");
     }
 }
